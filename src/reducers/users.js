9399ea434e7b253ecf55621a9746204b7380ef53
Samuel Cha <sunchipssam@gmail.com>
2018-02-28 16:35:32 -0800

    Added dep and started before pp

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,6 +3,7 @@ import { GET_USERS } from '../actions';
 export default (users = [], action) => {
   switch (action.type) {
     case GET_USERS:
+      console.log(action.payload);
       return action.payload;
     default:
       return users;
